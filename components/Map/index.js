d626807a1d338511601306e4899560b5c0e71e42
Bernardo Dias <bernardodiasdacruz@gmail.com>
2021-11-22 11:04:06 -0300

    Setup Google Maps

---
+++ components/Map/index.js
@@ -0,0 +1,12 @@
+import GoogleMap from './GoogleMap'
+
+// import dynamic from 'next/dynamic'
+// const LeafletMap = dynamic(
+//   () => import('./LeafletMap'),
+//   {
+//     loading: () => <p>A map is loading</p>,
+//     ssr: false
+//   }
+// )
+
+export default GoogleMap
