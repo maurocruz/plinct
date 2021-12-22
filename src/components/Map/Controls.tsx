f91e33231e480c1f633bac5a5360c8da8673f0a1
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-12-22 12:37:39 -0300

    Added change tileset street to sattelite

--- src/components/Map/Controls.tsx
+++ src/components/Map/Controls.tsx
@@ -1,8 +1,73 @@
 import { Icon } from "@iconify/react"
+import { CSSProperties, useState } from "react"
 import { NavigationControl } from "react-map-gl"
 import useAppContext from "../../contexts/App"
 import useLocation from "../../hooks/useLocation/useLocation"
 
+/**
+ * MUDA O TILESET (FUNDO DO MAPA)
+ * @param param0 
+ * @returns 
+ */
+const ChangeTileset = ({mapStyle, setMapStyle, mapStreet, mapSattelite}) => 
+{
+    const imageStreet = "/media/streets-style-icon.6cda255c.png";
+    const textStreet = "Streets";
+    const imageSattelite = "/media/satellite-style-icon.f45844d1.png";
+    const textSattelite = "Sattelite";
+
+    const [ imgBackground, setImgBackground ] = useState(imageSattelite)
+    const [ textSpan, setTextSpan ] = useState(textSattelite)
+
+    const buttonStyle: CSSProperties = {
+        position: 'absolute',
+        right: '24px',
+        bottom: '24px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-end',
+        width: '60px',
+        height: '60px',
+        boxSizing: 'initial',
+        boxShadow: '0 2px 4px rgba(21,45,72,.25)',
+        backgroundSize: 'cover',
+        borderRadius: '8px',
+        border: '2px solid #fff',
+        backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0) 28.47%), url(${imgBackground})`,
+        cursor: 'pointer'
+    }
+
+    const spanStyle: CSSProperties = {
+        marginLeft: '3px',
+        fontSize: '12px',
+        lineHeight: '16px',
+        color: '#fff'
+    }
+
+    function _onClick() {
+        if(mapStyle == mapStreet) {
+            setMapStyle(mapSattelite);
+            setImgBackground(imageStreet);
+            setTextSpan(textStreet);
+        } else {
+            setMapStyle(mapStreet);
+            setImgBackground(imageSattelite);
+            setTextSpan(textSattelite);
+        }
+    }
+
+    return (
+        <button style={buttonStyle} onClick={_onClick}>
+            <span style={spanStyle}>{textSpan}</span>
+        </button>        
+    )
+}
+
+/**
+ * CONTROLE DE ZOOM
+ * @returns 
+ */
 const Navigation = () => {
     return (
         <NavigationControl 
@@ -12,6 +77,10 @@ const Navigation = () => {
     )
 }
 
+/**
+ * BOTÃO PARA VOAR À LOCALIZAÇÃO DO USUARIO
+ * @returns 
+ */
 const UserLocation = () => {
 
     const { setData } = useAppContext()
@@ -36,4 +105,4 @@ const UserLocation = () => {
     )
 }
 
-export { Navigation, UserLocation }
+export { Navigation, UserLocation, ChangeTileset }
--- src/components/Map/MapBox.tsx
+++ src/components/Map/MapBox.tsx
@@ -1,10 +1,5 @@
-/**
- * 
- * COMPONENT MAP EM MAPBOX
- * 
- */
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactMapGL, { MapEvent } from 'react-map-gl';
 
 import TooltipRightButton from "./TooltipRightButton";
@@ -13,26 +8,25 @@ import Points from "./Points";
 
 import DataInterface from "../../interfaces/DataInterface";
 import { FeatureInterface } from "../../interfaces/geoJson/GeoJsonInterface";
-import { Navigation, UserLocation } from "./Controls";
+import { ChangeTileset, Navigation, UserLocation } from "./Controls";
 
 /**  
+ * COMPONENT MAP EM MAPBOX
+ * 
  * @param param0 
  * @returns 
  */
 const MapBox = ({ data }) => 
 {    
     const DATA = data as DataInterface;
-    //const [ DATA, setDATA ] = useState<DataInterface>(data);
+
+    const mapStreet = "mapbox://styles/maurocruz/ckur99bp404ze15o0icj8kt6h"
+    const mapSattelite = "mapbox://styles/maurocruz/ckxgf6qdl0p4g14rrqmkr7vh5"
 
     const [ viewport, setViewport ] = useState(data.viewport);
     const [ rightButton, setRightButton ] = useState<MapEvent>(null)
     const [ eventInfo, setEventInfo ] = useState(null);
-
-
-    const reactMap = useRef();
-
-    // for get map size
-    const mapContainer = useRef<HTMLDivElement>()
+    const [ mapStyle, setMapStyle ] = useState(mapStreet)
 
     useEffect(()=>{
         setViewport(data.viewport);
@@ -43,49 +37,39 @@ const MapBox = ({ data }) =>
             setRightButton(e);
         }
     }
-    
-    // street style
-    // mapbox://styles/maurocruz/ckur99bp404ze15o0icj8kt6h
-    // satellite style
-    // mapbox://styles/maurocruz/ckxgf6qdl0p4g14rrqmkr7vh5
-
-    //<UserLocation setDATA={SetDATA} />
-
-    //console.log(DATA.geojson.features)
 
     return (
-        <div ref={mapContainer}>
-            <ReactMapGL
-                ref={reactMap}
-                mapStyle="mapbox://styles/maurocruz/ckur99bp404ze15o0icj8kt6h"
-                mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
-                {...viewport}
-                width="100%"
-                height="100%"
-                onViewportChange={(viewport: any) => setViewport(viewport)}
-                onClick={_onClick}      
-            >
-                
-                {DATA.geojson.features.map((feature: FeatureInterface) => { 
-                    return <Points key={feature.properties.id} feature={feature} /> 
-                })}
+        <ReactMapGL
+            mapStyle={mapStyle} 
+            mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+            {...viewport}
+            width="100%"
+            height="100%"
+            onViewportChange={(viewport: any) => setViewport(viewport)}
+            onClick={_onClick}      
+        >
+            
+            {DATA.geojson.features.map((feature: FeatureInterface) => { 
+                return <Points key={feature.properties.id} feature={feature} /> 
+            })}
 
-                <UserLocation />
+            <UserLocation />
 
-                <Navigation />
+            <Navigation />
 
-                {rightButton && 
-                    <TooltipRightButton 
-                        mapEvent={rightButton} 
-                        setRightButton={setRightButton} 
-                        setEventInfo={setEventInfo}
-                    />
-                }
+            {rightButton && 
+                <TooltipRightButton 
+                    mapEvent={rightButton} 
+                    setRightButton={setRightButton} 
+                    setEventInfo={setEventInfo}
+                />
+            }
 
-                {eventInfo && <EventInfo>{eventInfo}</EventInfo>}
+            {eventInfo && <EventInfo>{eventInfo}</EventInfo>}
 
-            </ReactMapGL>
-        </div>
+            <ChangeTileset mapStyle={mapStyle} setMapStyle={setMapStyle} mapStreet={mapStreet} mapSattelite={mapSattelite} />
+
+        </ReactMapGL>
     )
 }
 
